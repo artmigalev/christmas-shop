4d41dea058b39921938b8a930e58ea5011a5bf60
artmigalev <art_migalev@yahoo.com>
2024-11-27 17:53:35 +0300

    feat: Implementation of the burger menu on both pages

---
+++ christmas-shop/js/index.js
@@ -0,0 +1,14 @@
+import { handleBurger, burger, item, closeMenu } from './components/burger.js';
+
+const mediaQuery = window.matchMedia('(min-width:769px)');
+//* --------------burger and burger-menu----------------- */
+mediaQuery.addEventListener('change',(event) => {
+  if (event.matches) {
+    if (burger.classList.contains('burger-icon-active')) {
+      closeMenu();
+    }
+  }
+});
+
+burger.addEventListener('click', handleBurger);
+item.addEventListener('click', closeMenu);
